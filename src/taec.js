6bfea790009ef5e9288eb64203e02537337746a6
Sasha <qiiq@live.com>
2023-12-20 04:14:05 -0700

    added comments

--- src/taec.js
+++ src/taec.js
@@ -1,11 +1,26 @@
+
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns a + b
+ */
 let sum = (a, b) => {
     return a + b;
 }
 
+// -----------------------
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns a - b
+ */
 let subtract = (a, b) => {
     return a - b;
 }
 
+// -----------------------
 module.exports = {
     sum,
     subtract
